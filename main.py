04bbc3d69402e3086b8c3939dcae7b973b15e452
aviv9913 <aviv9913@gmail.com>
2021-05-31 20:11:24 +0300

    added renaming function

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from Processor import Processor
 from PIL import Image
 import numpy as np
 import os
+import shutil
+import os
 
 
 def test_processor():
@@ -26,7 +28,7 @@ def test_processor():
 
 def process_folder(path):
     not_images = 0
-    for filename in os.listdir(path):
+    for filename in sorted(os.listdir(path)):
         if filename.endswith(".png"):
             filename_without_suffix = os.path.splitext(filename)[0]
             image_path = os.path.join(path, filename)
@@ -44,9 +46,40 @@ def process_folder(path):
     print("total number of files processed:", len(os.listdir(path)) - not_images)
 
 
+def rename_images(path):
+    index = 0
+    images_in_path = [float((''.join(f.split('_')[0])[5:])) for f in os.listdir(path) if f.endswith(".png")]
+    for filename in sorted(os.listdir(path), key=lambda f: float((''.join(f.split('_')[0])[5:]))):
+        print("renaming layer image ", filename)
+        image_path = os.path.join(path, filename)
+        final_folder = os.path.join(path, "Processed_with_renaming")
+
+        try:
+            os.mkdir(final_folder)
+        except FileExistsError:
+            pass
+
+        str_index = f'{index:05}'
+        prefix = 'prototype_'
+        final_path = os.path.join(final_folder, prefix + str_index + ".png")
+        if filename == "Value8.228_Top_processed.png":
+            print("copying layer 8.228")
+            for i in range(60):
+                shutil.copyfile(image_path, final_path)
+                index += 1
+                str_index = f'{index:05}'
+                final_path = os.path.join(final_folder, prefix + str_index + ".png")
+        else:
+            shutil.copyfile(image_path, final_path)
+            index += 1
+    print("total number of files processed:", index+1)
+
+
+
 if __name__ == '__main__':
-    pass
-    # images_folder_path = r'C:\Users\Aviv\Downloads\prototype 02'
+    path = r'C:\Users\aviv\Downloads\prototype 02\Processed\Processed_with_renaming'
+    print(os.listdir(path))
     # start = time.time()
     # process_folder(images_folder_path)
     # print("all execution took: ", time.time()-start, " sec")
+    # rename_images(path)
